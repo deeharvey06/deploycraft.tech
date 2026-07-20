f7174f098b5537a768ca4f484d0185f59e94be85
deeharvey06 <cliffharvey06@gmail.com>
2026-07-20 10:21:57 -0700

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://deeharvey06.github.io/deploycraft.tech/', // Replace with your desired base path
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+});
