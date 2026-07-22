60decee026513c017340ceb4cbd20e6a11a4b550
deeharvey06 <cliffharvey06@gmail.com>
2026-07-22 12:48:51 -0700

    import clean up

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { Layout } from '@/components/Layout';
 import { Home } from '@/pages/Home';
 import { About } from '@/pages/About';
 import { Insights } from '@/pages/Insights';
 import { Contact } from '@/pages/Contact';
+
 import './index.css';
 
 const queryClient = new QueryClient();
